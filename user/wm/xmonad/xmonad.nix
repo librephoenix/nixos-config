c6dc7aef60ec96180b3358297e00d2a96b313e6a
Emmet <librephoenix3@pm.me>
2023-05-09 16:13:32 -0500

    Locked base-xmobarrc and startup to nix

--- user/wm/xmonad/xmonad.nix
+++ user/wm/xmonad/xmonad.nix
@@ -1,3 +1,7 @@
 { config, pkgs, ... }:
 
-{ home.file.".config/xmonad/xmonad.hs".source = ./xmonad.hs; }
+{
+  home.file.".config/xmonad/xmonad.hs".source = ./xmonad.hs;
+  home.file.".config/xmobar/base-xmobarrc".source = ./base-xmobarrc;
+  home.file.".config/xmonad/startup.sh".source = ./startup.sh;
+}
