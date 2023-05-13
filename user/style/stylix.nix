1eee403e6c1b5073a508ea4e5f822b3a9a03957a
Emmet <librephoenix3@pm.me>
2023-05-13 14:36:06 -0500

    Configured stylix fonts

--- user/style/stylix.nix
+++ user/style/stylix.nix
@@ -1,5 +1,10 @@
 { config, pkgs, ... }:
 
+let
+  myFont = "Inconsolata";
+  myFontPkg = pkgs.inconsolata;
+
+in
 {
   stylix.autoEnable = false;
   stylix.image = pkgs.fetchurl {
@@ -7,6 +12,32 @@
     sha256 = "+xl4H3UiVmMRNvMhIlaLdVTYYqnSyCTSX2UOTGsDQ8c=";
   };
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+
+  stylix.fonts = {
+    monospace = {
+      name = myFont;
+      package = myFontPkg;
+    };
+    serif = {
+      name = myFont;
+      package = myFontPkg;
+    };
+    sansSerif = {
+      name = myFont;
+      package = myFontPkg;
+    };
+    emoji = {
+      name = "Noto Color Emoji";
+      package = pkgs.noto-fonts-emoji-blob-bin;
+    };
+    sizes = {
+      terminal = 18;
+      applications = 14;
+      popups = 12;
+      desktop = 12;
+    };
+  };
+
   stylix.targets.alacritty.enable = true;
   #programs.alacritty.enable = true;
   stylix.targets.kitty.enable = true;
