890dd7c339eaabd85067230db11c63b250f02c5a
Emmet <librephoenix3@pm.me>
2023-05-16 19:12:50 -0500

    Added stylix colors to xmonad

--- user/wm/xmonad/startup.sh
+++ user/wm/xmonad/startup.sh
@@ -1,37 +1,7 @@
-#!/bin/sh
-
-trayertint=$1
-
-nbColor=$2
-nfColor=$3
-sbColor=$4
-sfColor=$5
-
-themeGTKName=$6
-themeAlacrittyName=$7
-themeDoomEmacsName=$8
-
-colorBgNormal=$2
-colorBgBright=${27}
-colorFgNormal=$3
-color01Normal=$9
-color01Bright=${10}
-color02Normal=${11}
-color02Bright=${12}
-color03Normal=${13}
-color03Bright=${14}
-color04Normal=${15}
-color04Bright=${16}
-color05Normal=${17}
-color05Bright=${18}
-color06Normal=${19}
-color06Bright=${20}
-color07Normal=${21}
-color07Bright=${22}
-color08Normal=${23}
-color08Bright=${24}
-colorFocus=${25}
-colorSecondary=${26}
+colorBg=$1
+colorFg=$2
+colorFocus=$3
+colorSecondary=$4
 
 # Startup shell script called by xmonad to start necessary programs
 #
@@ -53,7 +23,7 @@ xset r rate 350 50 &
 setxkbmap -option caps:escape &
 ~/.fehbg &
 twmnd &
-alttab -w 1 -t 240x160 -i 64x64 -sc 1 -bg $colorBgNormal -fg $colorFgNormal -frame $colorSecondary -inact $colorFgNormal &
+alttab -w 1 -t 240x160 -i 64x64 -sc 1 -bg $colorBg -fg $colorFg -frame $colorSecondary -inact $colorFg &
 autokey-gtk &
 ##/usr/bin/trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --widthtype request --transparent true --alpha 0 --height 28 --tint $trayertint --monitor "primary" &
 nm-applet &
--- user/wm/xmonad/xmonad.nix
+++ user/wm/xmonad/xmonad.nix
@@ -7,4 +7,14 @@
   home.file.".config/xmonad/xmonad.hs".source = ./xmonad.hs;
   home.file.".config/xmobar/base-xmobarrc".source = ./base-xmobarrc;
   home.file.".config/xmonad/startup.sh".source = ./startup.sh;
+
+  home.file.".config/xmonad/lib/Colors/Stylix.hs".source = config.lib.stylix.colors {
+    template = builtins.readFile ./lib/Colors/Stylix.hs.mustache;
+    extension = ".hs";
+  };
+
+  home.packages = with pkgs; [
+    xmobar
+    dunst
+  ];
 }
