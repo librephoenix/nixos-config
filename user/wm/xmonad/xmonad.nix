99fdb57de9e67bed50c2cb56bb92e6a580d21fc0
Emmet <librephoenix3@pm.me>
2023-05-16 19:44:57 -0500

    Organized xmonad color config + added stylix xmobar

--- user/wm/xmonad/xmonad.nix
+++ user/wm/xmonad/xmonad.nix
@@ -5,12 +5,16 @@
   imports = [ ../picom/picom.nix ];
 
   home.file.".config/xmonad/xmonad.hs".source = ./xmonad.hs;
-  home.file.".config/xmobar/base-xmobarrc".source = ./base-xmobarrc;
   home.file.".config/xmonad/startup.sh".source = ./startup.sh;
 
   home.file.".config/xmonad/lib/Colors/Stylix.hs".source = config.lib.stylix.colors {
     template = builtins.readFile ./lib/Colors/Stylix.hs.mustache;
     extension = ".hs";
+  };
+
+  home.file.".config/xmobar/xmobarrc".source = config.lib.stylix.colors {
+    template = builtins.readFile ./xmobarrc.mustache;
+    extension = "";
   };
 
   home.packages = with pkgs; [
