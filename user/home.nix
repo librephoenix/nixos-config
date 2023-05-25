63d3ba55b04a12dd28c33af5e3ef7f1d6ec81719
Emmet <librephoenix3@pm.me>
2023-05-24 20:22:27 -0500

    Fixed my librewolf config and opacity

--- user/home.nix
+++ user/home.nix
@@ -34,7 +34,7 @@
     zsh
     alacritty
     kitty
-    librewolf-wayland
+    librewolf
     brave
     dmenu
     rofi
@@ -147,6 +147,16 @@
   ];
 
   home.file.".librewolf/librewolf.overrides.cfg".text = ''
+    defaultPref("font.name.serif.x-western","Inconsolata");
+    defaultPref("font.size.variable.x-western",20);
+    defaultPref("browser.toolbars.bookmarks.visibility","always");
+    defaultPref("privacy.resisttFingerprinting.letterboxing", true);
+    defaultPref("network.http.referer.XOriginPolicy",2);
+    defaultPref("privacy.clearOnShutdown.history",false);
+    defaultPref("privacy.clearOnShutdown.downloads",false);
+    defaultPref("privacy.clearOnShutdown.cookies",false);
+    defaultPref("gfx.webrender.software.opengl",true);
+    defaultPref("webgl.disabled",false);
     pref("font.name.serif.x-western","Inconsolata");
     pref("font.size.variable.x-western",20);
     pref("browser.toolbars.bookmarks.visibility","always");
@@ -156,7 +166,7 @@
     pref("privacy.clearOnShutdown.downloads",false);
     pref("privacy.clearOnShutdown.cookies",false);
     pref("gfx.webrender.software.opengl",true);
-    pref("webgl"webgl.disabled",false);
+    pref("webgl.disabled",false);
     '';
 
   home.file.".config/networkmanager-dmenu/config.ini".text = ''
