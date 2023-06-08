cfb1187e760351fbbe27b691f1c4838909e75464
Emmet <librephoenix3@pm.me>
2023-06-08 15:44:40 -0500

    Applied stylix to qt apps

--- user/style/stylix.nix
+++ user/style/stylix.nix
@@ -56,4 +56,30 @@ in
   }+'';
   '';
   home.file.".fehbg-stylix".executable = true;
+  home.file = {
+    ".config/qt5ct/colors/oomox-current.conf".source = config.lib.stylix.colors {
+      template = builtins.readFile ./oomox-current.conf.mustache;
+      extension = ".conf";
+    };
+    ".config/Trolltech.conf".source = config.lib.stylix.colors {
+      template = builtins.readFile ./Trolltech.conf.mustache;
+      extension = ".conf";
+    };
+    ".config/kdeglobals".source = config.lib.stylix.colors {
+      template = builtins.readFile ./Trolltech.conf.mustache;
+      extension = "";
+    };
+    ".config/qt5ct/qt5ct.conf".text = pkgs.lib.mkBefore (builtins.readFile ./qt5ct.conf);
+  };
+  home.packages = with pkgs; [
+     qt5ct pkgs.libsForQt5.breeze-qt5
+  ];
+  home.sessionVariables = {
+    QT_QPA_PLATFORMTHEME="qt5ct";
+  };
+  qt = {
+    enable = true;
+    style.package = pkgs.libsForQt5.breeze-qt5;
+    style.name = "breeze-dark";
+  };
 }
