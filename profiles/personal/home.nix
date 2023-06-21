ef09b4302a11454955e45fe2da8d6e3213029f15
Emmet <librephoenix3@pm.me>
2023-06-21 09:33:51 -0500

    Created work profile

--- profiles/personal/home.nix
+++ profiles/personal/home.nix
@@ -8,24 +8,8 @@
 
   programs.home-manager.enable = true;
 
-  imports = [
-              nix-doom-emacs.hmModule
-              stylix.homeManagerModules.stylix
-              ../../user/wm/xmonad/xmonad.nix # My xmonad config
-              ../../user/shell/sh.nix # My zsh and bash config
-              ../../user/shell/cli-collection.nix # Useful CLI apps
-              ../../user/bin/phoenix.nix # My nix command wrapper
-              ../../user/app/doom-emacs/doom.nix # My doom emacs config
-              ../../user/app/ranger/ranger.nix # My ranger file manager config
-              ../../user/app/git/git.nix # My git config
-              ../../user/app/keepass/keepass.nix # My password manager
-              ../../user/app/browser/librewolf.nix # My default browser
+  imports = [ ../work/home.nix # Personal is essentially work system + games
               ../../user/app/games/games.nix # Various videogame apps
-              ../../user/app/virtualization/virtualization.nix # Virtual machines
-              ../../user/app/flatpak/flatpak.nix # Flatpaks
-              ../../user/style/stylix.nix # Styling and themes for my apps
-              ../../user/lang/cc/cc.nix # C and C++ tools
-              ../../user/lang/godot/godot.nix # Game development
             ];
 
   home.stateVersion = "22.11"; # Please read the comment before changing.
@@ -41,71 +25,16 @@
     git
     syncthing
 
-    # Office
-    libreoffice-qt
-    mate.atril
-    xournalpp
-    glib
-    gnome.geary
-    gnome.gnome-calendar
-    gnome.seahorse
-    gnome.gnome-maps
-    newsflash
-    #autokey
-    protonmail-bridge
-    openvpn
-
     # Media
-    gimp-with-plugins
-    krita
-    musikcube
-    vlc
-    mpv
-    yt-dlp
-    freetube
-    blender
-    obs-studio
-    libsForQt5.kdenlive
-    movit
-    mediainfo
-    libmediainfo
-    mediainfo-gui
-    audio-recorder
     gtkcord4
-
-    # Various dev packages
-    texinfo
-    libffi zlib
-    nodePackages.ungit
-
   ];
-
-  services.syncthing.enable = true;
 
   xdg.enable = true;
   xdg.userDirs = {
-    enable = true;
-    createDirectories = true;
-    music = "${config.home.homeDirectory}/Media/Music";
-    videos = "${config.home.homeDirectory}/Media/Videos";
-    pictures = "${config.home.homeDirectory}/Media/Pictures";
-    templates = "${config.home.homeDirectory}/Templates";
-    download = "${config.home.homeDirectory}/Downloads";
-    documents = "${config.home.homeDirectory}/Documents";
-    desktop = null;
-    publicShare = null;
     extraConfig = {
-      XDG_DOTFILES_DIR = "${config.home.homeDirectory}/.dotfiles";
-      XDG_ARCHIVE_DIR = "${config.home.homeDirectory}/Archive";
-      XDG_VM_DIR = "${config.home.homeDirectory}/Machines";
-      XDG_ORG_DIR = "${config.home.homeDirectory}/Org";
       XDG_GAME_DIR = "${config.home.homeDirectory}/Media/Games";
       XDG_GAME_SAVE_DIR = "${config.home.homeDirectory}/Media/Game Saves";
-      XDG_PODCAST_DIR = "${config.home.homeDirectory}/Media/Podcasts";
-      XDG_BOOK_DIR = "${config.home.homeDirectory}/Media/Books";
     };
   };
-  xdg.mime.enable = true;
-  xdg.mimeApps.enable = true;
 
 }
---
+++ profiles/work/configuration.nix
@@ -0,0 +1,87 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, lib, pkgs, blocklist-hosts, myName, myHostname, myTimezone, myLocale, myTheme, myBackgroundUrl, myBackgroundSha256, ... }:
+{
+  imports =
+    [ ../../system/hardware-configuration.nix
+      ../../system/hardware/power.nix
+      ../../system/hardware/opengl.nix
+      ../../system/hardware/printing.nix
+      ../../system/hardware/bluetooth.nix
+      ../../system/wm/xmonad.nix
+      ../../system/app/flatpak.nix
+      ../../system/security/doas.nix
+      ../../system/security/gpg.nix
+      ../../system/security/blocklist.nix
+      ../../system/security/firewall.nix
+      ../../system/security/openvpn.nix
+      ../../system/style/stylix.nix
+    ];
+
+  # Fix nix path
+  nix.nixPath = [ "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
+                  "nixos-config=$HOME/dotfiles/system/configuration.nix"
+                  "/nix/var/nix/profiles/per-user/root/channels"
+                ];
+
+  # Experimental features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # I'm sorry Stallman-taichou
+  nixpkgs.config.allowUnfree = true;
+
+  # Kernel modules
+  boot.kernelModules = [ "i2c-dev" "i2c-piix4" "cpufreq_powersave" ];
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  # Networking
+  networking.hostName = myHostname; # Define your hostname.
+  networking.networkmanager.enable = true; # Use networkmanager
+
+  # Timezone and locale
+  time.timeZone = myTimezone; # time zone
+  i18n.defaultLocale = myLocale;
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = myLocale;
+    LC_IDENTIFICATION = myLocale;
+    LC_MEASUREMENT = myLocale;
+    LC_MONETARY = myLocale;
+    LC_NAME = myLocale;
+    LC_NUMERIC = myLocale;
+    LC_PAPER = myLocale;
+    LC_TELEPHONE = myLocale;
+    LC_TIME = myLocale;
+  };
+
+  # User account
+  users.users.${myName} = {
+    isNormalUser = true;
+    description = "Emmet";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+    uid = 1000;
+  };
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    zsh
+    git
+  ];
+
+  # I use zsh btw
+  environment.shells = with pkgs; [ zsh ];
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh.enable = true;
+
+  # It is ok to leave this unchanged for compatibility purposes
+  system.stateVersion = "22.11";
+
+}
