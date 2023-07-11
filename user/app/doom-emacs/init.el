ab5f4291b2a22c6e30a65fdb37175097c811a5b1
Emmet <librephoenix3@pm.me>
2023-07-10 21:32:21 -0500

    Docker is kinda nice

--- user/app/doom-emacs/init.el
+++ user/app/doom-emacs/init.el
@@ -72,7 +72,7 @@
        ;;biblio            ; Writes a PhD for you (citation needed)
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
+       docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
