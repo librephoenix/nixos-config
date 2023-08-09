0be1e9f60fcb4e8747e8dce0d7a618eb6f27ab3a
Emmet <librephoenix3@pm.me>
2023-08-09 11:27:34 -0500

    Better power/performance on battery power

--- system/hardware/power.nix
+++ system/hardware/power.nix
@@ -1,8 +1,23 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = [ pkgs.auto-cpufreq ];
+  environment.systemPackages = with pkgs; [ auto-cpufreq thermald ];
   systemd.services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    charger = {
+      governor = "performance";
+      turbo = "auto";
+    };
+    battery = {
+      governor = "powersave";
+      scaling_min_freq = 1200000;
+      scaling_max_freq = 2200000;
+      turbo = "never";
+    };
+  };
+
+  services.thermald.enable = true;
 
   services.upower.enable = true;
   systemd.services.upower.enable = true;
