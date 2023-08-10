1600681cfcb77df82354523eef9519af184264a2
Emmet <librephoenix3@pm.me>
2023-08-10 07:56:06 -0500

    Tramp for emacs and shell setup for remote servers

--- user/app/doom-emacs/config.el
+++ user/app/doom-emacs/config.el
@@ -1055,3 +1055,13 @@ https://github.com/magit/magit/issues/460 (@cpitclaudel)."
 (use-package gdscript-mode
   :hook (gdscript-mode . lsp-deferred)
   :ensure t)
+
+(after! tramp
+  (setq tramp-inline-compress-start-size 1000)
+  (setq tramp-copy-size-limit 10000)
+  (setq vc-handled-backends '(Git))
+  (setq tramp-verbose 1)
+  (setq tramp-default-method "scp")
+  (setq tramp-use-ssh-controlmaster-options nil)
+  (setq projectile--mode-line "Projectile")
+  (setq tramp-verbose 1))
--- user/shell/sh.nix
+++ user/shell/sh.nix
@@ -21,6 +21,7 @@ in
     PROMPT=" ◉ %U%F{magenta}%n%f%u@%U%F{blue}%m%f%u:%F{yellow}%~%f
      %F{green}→%f "
     RPROMPT="%F{red}▂%f%F{yellow}▄%f%F{green}▆%f%F{cyan}█%f%F{blue}▆%f%F{magenta}▄%f%F{white}▂%f"
+    [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
     '';
   };
 
