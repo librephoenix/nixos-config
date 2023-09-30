90ea1f4186b81a08903621fd148b15ad59a6b83c
Emmet <librephoenix3@pm.me>
2023-09-30 11:58:02 -0500

    Setup flstudio via bottles/wine

--- profiles/work/home.nix
+++ profiles/work/home.nix
@@ -54,6 +54,30 @@
     openvpn
     protonmail-bridge
 
+    wine
+    bottles
+    # The following requires 64-bit FL Studio (FL64) to be installed to a bottle
+    # With a bottle name of "FL Studio"
+    (pkgs.writeShellScriptBin "flstudio" ''
+       #!/bin/sh
+       if [ -z "$1" ]
+         then
+           bottles-cli run -b "FL Studio" -p FL64
+           #flatpak run --command=bottles-cli com.usebottles.bottles run -b FL\ Studio -p FL64
+         else
+           filepath=$(winepath --windows "$1")
+           echo \'"$filepath"\'
+           bottles-cli run -b "FL Studio" -p "FL64" --args \'"$filepath"\'
+           #flatpak run --command=bottles-cli com.usebottles.bottles run -b FL\ Studio -p FL64 -args "$filepath"
+         fi
+    '')
+    (pkgs.makeDesktopItem {
+      name = "flstudio";
+      desktopName = "FL Studio 64";
+      exec = "flstudio";
+      terminal = false;
+    })
+
     # Media
     gimp-with-plugins
     pinta
