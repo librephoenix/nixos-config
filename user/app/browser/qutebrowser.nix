9956b987a2af2c29575d9db8bde6a9eb24c24053
Emmet <librephoenix3@pm.me>
2023-09-30 21:24:38 -0500

    Makes qutebrowser primary browser wrt to xdg

--- user/app/browser/qutebrowser.nix
+++ user/app/browser/qutebrowser.nix
@@ -5,6 +5,13 @@
                     (pkgs.callPackage ./qute-containers.nix { dmenuCmd = "fuzzel -d"; })
                   ];
   home.sessionVariables = { DEFAULT_BROWSER = "${pkgs.qutebrowser}/bin/qutebrowser"; };
+  xdg.mimeApps.defaultApplications = {
+  "text/html" = "org.qutebrowser.qutebrowser.desktop";
+  "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
+  "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
+  "x-scheme-handler/about" = "org.qutebrowser.qutebrowser.desktop";
+  "x-scheme-handler/unknown" = "org.qutebrowser.qutebrowser.desktop";
+  };
   home.file.".config/qutebrowser/userscripts/container-open".source = "${(pkgs.callPackage ./qute-containers.nix { dmenuCmd = "fuzzel -d"; })}/bin/container-open";
   home.file.".config/qutebrowser/userscripts/containers_config".source = "${(pkgs.callPackage ./qute-containers.nix { dmenuCmd = "fuzzel -d"; })}/bin/containers_config";
 
