b4a6a2099d5dff5a63bb428cb979876ceca915fa
Emmet <librephoenix3@pm.me>
2023-10-13 18:36:34 -0500

    Updated some hyprprofiles

--- user/wm/hyprland/hyprprofiles/profiles/Personal/start-hook.sh
+++ user/wm/hyprland/hyprprofiles/profiles/Personal/start-hook.sh
@@ -2,3 +2,4 @@
 
 hyprctl keyword unbind SUPER,S;
 hyprctl keyword bind SUPER,S,exec,qutebrowser;
+emacsclient --eval '(org-roam-switch-db "Personal.p" t)'
--- user/wm/hyprland/hyprprofiles/profiles/Teaching/start-hook.sh
+++ user/wm/hyprland/hyprprofiles/profiles/Teaching/start-hook.sh
@@ -2,4 +2,4 @@
 
 hyprctl keyword unbind SUPER,S;
 hyprctl keyword bind SUPER,S,exec,container-open Teaching;
-emacsclient --eval '(org-roam-switch-db "Teaching.p")'
+emacsclient --eval '(org-roam-switch-db "Teaching.p" t)'
