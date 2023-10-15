842739e6d916bf2deec18a5ab73d8fbabae6e25a
Emmet <librephoenix3@pm.me>
2023-10-14 21:45:28 -0500

    Fixes youtube hyprprofile

--- user/wm/hyprland/hyprprofiles/profiles/Youtube/start-hook.sh
+++ user/wm/hyprland/hyprprofiles/profiles/Youtube/start-hook.sh
@@ -2,4 +2,4 @@
 
 hyprctl keyword unbind SUPER,S;
 hyprctl keyword bind SUPER,S,exec,container-open Tech;
-emacsclient --eval '(org-roam-switch-db "Producer.p/LibrePhoenix.p" t)'
+emacsclient --eval '(org-roam-switch-db "Producer.p\/LibrePhoenix.p" t)'
