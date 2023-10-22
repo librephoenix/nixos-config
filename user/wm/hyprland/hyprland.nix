bb89d4cbb5ceee72c06e1ac42952072abff10a54
Emmet <librephoenix3@pm.me>
2023-10-22 17:33:51 -0500

    Updated pyprland

--- user/wm/hyprland/hyprland.nix
+++ user/wm/hyprland/hyprland.nix
@@ -343,10 +343,10 @@
     '')
     (pkgs.python3Packages.buildPythonPackage rec {
       pname = "pyprland";
-      version = "1.4.0";
+      version = "1.4.1";
       src = pkgs.fetchPypi {
         inherit pname version;
-        sha256 = "sha256-gB/QkTbkr9VMzPPhubQNorPTS4Lm90TS1LCSGqPzPmU=";
+        sha256 = "sha256-JRxUn4uibkl9tyOe68YuHuJKwtJS//Pmi16el5gL9n8=";
       };
       format = "pyproject";
       propagatedBuildInputs = with pkgs; [
