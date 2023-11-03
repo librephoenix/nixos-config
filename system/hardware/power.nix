5af93d2b2d2c99a6f72122fa370434c1ee5438ab
Emmet <librephoenix3@pm.me>
2023-11-03 10:16:49 -0500

    Testing if auto-cpufreq is system freeze culprit

--- system/hardware/power.nix
+++ system/hardware/power.nix
@@ -1,18 +1,31 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ auto-cpufreq ];
-  systemd.services.auto-cpufreq.enable = true;
-  services.auto-cpufreq.enable = true;
-  services.auto-cpufreq.settings = {
-    charger = {
-      governor = "performance";
-      turbo = "auto";
-    };
-    battery = {
-      governor = "schedutil";
-      scaling_max_freq = 3800000;
-      turbo = "never";
+  #services.auto-cpufreq.enable = true;
+  #services.auto-cpufreq.settings = {
+  #  charger = {
+  #    governor = "performance";
+  #    turbo = "auto";
+  #  };
+  #  battery = {
+  #    governor = "schedutil";
+  #    scaling_max_freq = 3800000;
+  #    turbo = "never";
+  #  };
+  #};
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 50;
     };
   };
 
