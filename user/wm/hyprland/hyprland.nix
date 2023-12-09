9b97543653eaec0787cac201c6bef99ddbf6f25d
Emmet <emmet@librephoenix.com>
2023-12-08 19:03:46 -0600

    Improvements to waybar

--- user/wm/hyprland/hyprland.nix
+++ user/wm/hyprland/hyprland.nix
@@ -418,7 +418,6 @@
         position = "top";
         height = 35;
         margin = "7 7 3 7";
-        # width = 1280;
         spacing = 2;
 
         modules-left = [ "custom/os" "custom/hyprprofile" "battery" "backlight" "pulseaudio" "cpu" "memory" ];
@@ -448,28 +447,35 @@
             "7" = "󰄖";
             "8" = "󰑴";
             "9" = "󱎓";
+            "scratch_term" = "_";
+            "scratch_ranger" = "_󰴉";
+            "scratch_musikcube" = "_";
+            "scratch_btm" = "_";
+            "scratch_geary" = "_";
+            "scratch_pavucontrol" = "_󰍰";
           };
           "on-click" = "activate";
           "on-scroll-up" = "hyprctl dispatch workspace e+1";
           "on-scroll-down" = "hyprctl dispatch workspace e-1";
-          "all-outputs" = true;
-          "active-only" = true;
-          "ignore-workspaces" = "-";
-          "persistent-workspaces" = {
-              # this block doesn't seem to work for whatever reason
-              "eDP-1" = [1 2 3 4 5 6 7 8 9];
-              "DP-1" = [1 2 3 4 5 6 7 8 9];
-              "HDMI-A-1" = [1 2 3 4 5 6 7 8 9];
-              "1" = ["eDP-1" "DP-1" "HDMI-A-1"];
-              "2" = ["eDP-1" "DP-1" "HDMI-A-1"];
-              "3" = ["eDP-1" "DP-1" "HDMI-A-1"];
-              "4" = ["eDP-1" "DP-1" "HDMI-A-1"];
-              "5" = ["eDP-1" "DP-1" "HDMI-A-1"];
-              "6" = ["eDP-1" "DP-1" "HDMI-A-1"];
-              "7" = ["eDP-1" "DP-1" "HDMI-A-1"];
-              "8" = ["eDP-1" "DP-1" "HDMI-A-1"];
-              "9" = ["eDP-1" "DP-1" "HDMI-A-1"];
-          };
+          #"all-outputs" = true;
+          #"active-only" = true;
+          "ignore-workspaces" = ["scratch" "-"];
+          #"show-special" = false;
+          #"persistent-workspaces" = {
+          #    # this block doesn't seem to work for whatever reason
+          #    "eDP-1" = [1 2 3 4 5 6 7 8 9];
+          #    "DP-1" = [1 2 3 4 5 6 7 8 9];
+          #    "HDMI-A-1" = [1 2 3 4 5 6 7 8 9];
+          #    "1" = ["eDP-1" "DP-1" "HDMI-A-1"];
+          #    "2" = ["eDP-1" "DP-1" "HDMI-A-1"];
+          #    "3" = ["eDP-1" "DP-1" "HDMI-A-1"];
+          #    "4" = ["eDP-1" "DP-1" "HDMI-A-1"];
+          #    "5" = ["eDP-1" "DP-1" "HDMI-A-1"];
+          #    "6" = ["eDP-1" "DP-1" "HDMI-A-1"];
+          #    "7" = ["eDP-1" "DP-1" "HDMI-A-1"];
+          #    "8" = ["eDP-1" "DP-1" "HDMI-A-1"];
+          #    "9" = ["eDP-1" "DP-1" "HDMI-A-1"];
+          #};
         };
 
         "idle_inhibitor" = {
@@ -591,7 +597,7 @@
       }
 
       #workspaces button.visible {
-          color: #'' + config.lib.stylix.colors.base07 + '';
+          color: #'' + config.lib.stylix.colors.base05 + '';
       }
 
       #workspaces button.urgent {
