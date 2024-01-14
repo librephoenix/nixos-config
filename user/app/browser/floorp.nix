2d75530b6d95be66254e727323ddbc1a4b32b6be
Emmet <emmet@librephoenix.com>
2024-01-13 20:59:35 -0600

    Floorp is a pretty cool browser

---
+++ user/app/browser/floorp.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, wmType, font, ... }:
+
+{
+  # Module installing  as default browser
+  home.packages = [ pkgs.floorp ];
+
+  home.sessionVariables = {
+    DEFAULT_BROWSER = "${pkgs.floorp}/bin/floorp";
+  };
+
+  xdg.mimeApps.defaultApplications = {
+  "text/html" = "floorp.desktop";
+  "x-scheme-handler/http" = "floorp.desktop";
+  "x-scheme-handler/https" = "floorp.desktop";
+  "x-scheme-handler/about" = "floorp.desktop";
+  "x-scheme-handler/unknown" = "floorp.desktop";
+  };
+
+}
