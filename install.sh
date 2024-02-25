83a12f648611609e6a8f4d69d2e5187798e578a0
Emmet <emmet@librephoenix.com>
2024-02-25 12:31:46 -0600

    Another test via git

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 
 # Automated script to install my dotfiles
 
-nix-shell -p git --command "git clone https://gitlab.com/librephoenix/nixos-config ~/.dotfiles"
-sudo nixos-generate-config --show-hardware-config > ~/.dotfiles/system/hardware-configuration.nix
-if [ -z "$EDITOR" ]; then
-    EDITOR=nano;
-fi
-$EDITOR ~/.dotfiles/flake.nix;
-sudo nixos-rebuild switch --flake ~/.dotfiles#system;
-nix run home-manager/master -- switch --flake ~/.dotfiles#user;
-sudo ~/.dotfiles/harden.sh;
+echo "echo works"
+
+# nix-shell -p git --command "git clone https://gitlab.com/librephoenix/nixos-config ~/.dotfiles"
+# sudo nixos-generate-config --show-hardware-config > ~/.dotfiles/system/hardware-configuration.nix
+# if [ -z "$EDITOR" ]; then
+#     EDITOR=nano;
+# fi
+# $EDITOR ~/.dotfiles/flake.nix;
+# sudo nixos-rebuild switch --flake ~/.dotfiles#system;
+# nix run home-manager/master -- switch --flake ~/.dotfiles#user;
+# sudo ~/.dotfiles/harden.sh;
