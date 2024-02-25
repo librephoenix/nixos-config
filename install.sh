4d966ad749c641031501d46f0ef9c425215c8621
Emmet <emmet@librephoenix.com>
2024-02-25 13:50:11 -0600

    Extra failsafes and explanation for autoinstall

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@
 
 nix-shell -p git --command "git clone https://gitlab.com/librephoenix/nixos-config ~/.dotfiles"
 sudo nixos-generate-config --show-hardware-config > ~/.dotfiles/system/hardware-configuration.nix
+sed -i "0,/emmet/s//$(whoami)/" flake.nix
+sed -i "0,/Emmet/s//$(getent passwd $(whoami) | cut -d ':' -f 5 | cut -d ',' -f 1)/" flake.nix
 if [ -z "$EDITOR" ]; then
     EDITOR=nano;
 fi
 $EDITOR ~/.dotfiles/flake.nix;
 sudo nixos-rebuild switch --flake ~/.dotfiles#system;
-nix run home-manager/master -- switch --flake ~/.dotfiles#user;
+nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake ~/.dotfiles#user;
 sudo ~/.dotfiles/harden.sh;
