7f0daacd086c1c3bdfff25539fdba24481309d14
Emmet <emmet@librephoenix.com>
2024-03-09 11:28:18 -0600

    Reverting *some autoinstall script changes

--- flake.nix
+++ flake.nix
@@ -149,7 +149,7 @@
 
           install = pkgs.writeShellApplication {
             name = "install";
-            runtimeInputs = with pkgs; [ git gnused ];
+            runtimeInputs = with pkgs; [ git ]; # I could make this fancier by adding other deps
             text = ''${./install.sh} "$@"'';
           };
         });
