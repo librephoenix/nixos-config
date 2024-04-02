8e6c9b6340a1276178c8095fecf1df86f291fd98
Emmet <emmet@librephoenix.com>
2024-04-01 21:10:08 -0500

    Opting for performance while on battery

--- system/hardware/power.nix
+++ system/hardware/power.nix
@@ -19,13 +19,13 @@
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
 
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "performance";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 80;
+      CPU_MAX_PERF_ON_BAT = 100;
     };
   };
 
