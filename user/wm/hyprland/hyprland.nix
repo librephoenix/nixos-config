f0840a99ac803cf3d1257c7b334b252721e98746
Emmet <emmet@librephoenix.com>
2024-05-29 20:22:25 -0500

    Added and reorganized hyprland env vars

--- user/wm/hyprland/hyprland.nix
+++ user/wm/hyprland/hyprland.nix
@@ -35,6 +35,17 @@ in
       exec-once = dbus-update-activation-environment DISPLAY XAUTHORITY WAYLAND_DISPLAY
       exec-once = hyprctl setcursor '' + config.gtk.cursorTheme.name + " " + builtins.toString config.gtk.cursorTheme.size + ''
 
+      env = XDG_CURRENT_DESKTOP,Hyprland
+      env = XDG_SESSION_TYPE,wayland
+      env = XDG_SESSION_DESKTOP,Hyprland
+      env = WLR_DRM_DEVICES,/dev/dri/card2:/dev/dri/card1
+      env = GDK_BACKEND,wayland,x11,*
+      env = QT_QPA_PLATFORM,wayland;xcb
+      env = QT_QPA_PLATFORMTHEME,qt5ct
+      env = QT_AUTO_SCREEN_SCALE_FACTOR,1
+      env = QT_WAYLAND_DISABLE_WINDOWDECORATION,1
+      env = CLUTTER_BACKEND,wayland
+
       exec-once = hyprprofile Default
 
       exec-once = pypr
@@ -283,9 +294,6 @@ in
        xwayland {
          force_zero_scaling = true
        }
-
-       env = WLR_DRM_DEVICES,/dev/dri/card2:/dev/dri/card1
-       env = QT_QPA_PLATFORMTHEME,qt5ct
 
        binds {
          movefocus_cycles_fullscreen = false
