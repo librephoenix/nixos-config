87443f927f947b16c0b9b6628f0b1fbe1c9b974c
Emmet <emmet@librephoenix.com>
2024-06-15 20:38:31 -0500

    Removed unnecessary fonts

--- system/wm/fonts.nix
+++ system/wm/fonts.nix
@@ -4,14 +4,8 @@
   # Fonts are nice to have
   fonts.packages = with pkgs; [
     # Fonts
-    (nerdfonts.override { fonts = [ "Inconsolata" ]; })
+    nerdfonts
     powerline
-    inconsolata
-    inconsolata-nerdfont
-    iosevka
-    font-awesome
-    ubuntu_font_family
-    terminus_font
   ];
 
 }
