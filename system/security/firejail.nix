7851e1bda22f33e0c9afdadeafbc7929f4a0f89b
Emmet <emmet@librephoenix.com>
2024-06-23 20:23:43 -0500

    Disabling prism-launcher firejail temporarily

--- system/security/firejail.nix
+++ system/security/firejail.nix
@@ -4,10 +4,10 @@
   environment.systemPackages = with pkgs; [ firejail ];
   programs.firejail.enable = true;
   programs.firejail.wrappedBinaries = {
-    prismlauncher = {
-      executable = "${pkgs.prismlauncher}/bin/prismlauncher";
-      profile = ./firejail-profiles/prismlauncher.profile;
-    };
+    #prismlauncher = {
+    #  executable = "${pkgs.prismlauncher}/bin/prismlauncher";
+    #  profile = ./firejail-profiles/prismlauncher.profile;
+    #};
     steam = {
       executable = "${pkgs.steam}/bin/steam";
       profile = "${pkgs.firejail}/etc/firejail/steam.profile";
