b6f621c964e15e351bf76d094ba038f88d0dc3b7
Emmet <emmet@librephoenix.com>
2024-07-15 10:42:20 -0500

    Open ports for syncthing

--- system/security/firewall.nix
+++ system/security/firewall.nix
@@ -4,8 +4,8 @@
   # Firewall
   networking.firewall.enable = true;
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 22000 21027 ]; # syncthing
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ]; # syncthing
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 }
