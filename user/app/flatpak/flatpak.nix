617d5e970b3ba5219cc66ff19b6e1f298084bf3a
ponymushama <ponymushama@gmail.com>
2024-07-21 22:06:30 +0800

    disable flatpak

--- user/app/flatpak/flatpak.nix
+++ user/app/flatpak/flatpak.nix
@@ -6,7 +6,7 @@
     XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share"; # lets flatpak work
   };
 
-  services.flatpak.enable = true;
-  services.flatpak.packages = [ { appId = "com.kde.kdenlive"; origin = "flathub";  } ];
-  services.flatpak.update.onActivation = true;
+  #services.flatpak.enable = true;
+  #services.flatpak.packages = [ { appId = "com.kde.kdenlive"; origin = "flathub";  } ];
+  #services.flatpak.update.onActivation = true;
 }
