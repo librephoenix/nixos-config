92617265adeb6c82800fcdf842c668033574c935
Emmet <emmet@librephoenix.com>
2024-08-09 17:49:26 -0500

    Fix root owned home-manager sqlite errors

--- sync-user.sh
+++ sync-user.sh
@@ -6,6 +6,9 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# Fix root-owned sqlite errors
+sudo chown -R 1000:users ~/.cache/nix;
+
 # Install and build home-manager configuration
 home-manager switch --flake $SCRIPT_DIR#user;
 
