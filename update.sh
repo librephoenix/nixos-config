08c87b5f23f4a542b5d860303ca8ccfce847f6b8
Emmet <emmet@librephoenix.com>
2024-08-17 22:48:18 -0500

    Fix update script & update channels

--- update.sh
+++ update.sh
@@ -6,4 +6,8 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Update flake
-sudo nix flake update $SCRIPT_DIR;
+pushd $SCRIPT_DIR &> /dev/null;
+sudo nix flake update;
+sudo nix-channel --update;
+nix-channel --update;
+popd &> /dev/null;
