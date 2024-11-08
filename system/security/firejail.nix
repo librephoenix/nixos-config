0c5037333f7bfec48496680c6b144a5107fe5eeb
Emmet <emmet@librephoenix.com>
2024-11-08 14:42:12 -0600

    Turned off broken firejail profiles, needs fixing

--- system/security/firejail.nix
+++ system/security/firejail.nix
@@ -8,13 +8,13 @@
     #  executable = "${pkgs.prismlauncher}/bin/prismlauncher";
     #  profile = ./firejail-profiles/prismlauncher.profile;
     #};
-    steam = {
-      executable = "${pkgs.steam}/bin/steam";
-      profile = "${pkgs.firejail}/etc/firejail/steam.profile";
-    };
-    steam-run = {
-      executable = "${pkgs.steam}/bin/steam-run";
-      profile = "${pkgs.firejail}/etc/firejail/steam.profile";
-    };
+    #steam = {
+    #  executable = "${pkgs.steam}/bin/steam";
+    #  profile = "${pkgs.firejail}/etc/firejail/steam.profile";
+    #};
+    #steam-run = {
+    #  executable = "${pkgs.steam}/bin/steam-run";
+    #  profile = "${pkgs.firejail}/etc/firejail/steam.profile";
+    #};
   };
 }
