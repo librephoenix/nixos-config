be4639509d3eef0be9b67a64148de9a6a93e2353
Emmet <emmet@librephoenix.com>
2024-11-08 14:58:00 -0600

    Fixed cura desktop file

--- profiles/work/home.nix
+++ profiles/work/home.nix
@@ -125,23 +125,13 @@
       };
       extraPkgs = pkgs: with pkgs; [];
      })
-    #(pkgs-stable.cura.overrideAttrs (oldAttrs: {
-    #  postInstall = oldAttrs.postInstall + ''cp -rf ${(pkgs.makeDesktopItem {
-    #      name = "com.ultimaker.cura";
-    #      icon = "cura-icon";
-    #      desktopName = "Cura";
-    #      exec = "env QT_QPA_PLATFORM=xcb ${pkgs-stable.cura}/bin/cura %F";
-    #      tryExec = "env QT_QPA_PLATFORM=xcb ${pkgs-stable.cura}/bin/cura";
-    #      terminal = false;
-    #      type = "Application";
-    #      categories = ["Graphics"];
-    #      mimeTypes = ["model/stl" "application/vnd.ms-3mfdocument" "application/prs.wavefront-obj"
-    #                   "image/bmp" "image/gif" "image/jpeg" "image/png" "text/x-gcode" "application/x-amf"
-    #                   "application/x-ply" "application/x-ctm" "model/vnd.collada+xml" "model/gltf-binary"
-    #                   "model/gltf+json" "model/vnd.collada+xml+zip"];
-    #      })}/share/applications $out/share'';
-    #}))
-    #(pkgs.writeShellScriptBin "curax" ''env QT_QPA_PLATFORM=xcb ${pkgs-stable.cura}/bin/cura $@'')
+    (pkgs.makeDesktopItem {
+      name = "Cura";
+      desktopName = "Cura";
+      exec = "env QT_QPA_PLATFORM=xcb Cura %i";
+      terminal = false;
+      type = "Application";
+     })
     (pkgs-stable.curaengine_stable)
     openscad
     (stdenv.mkDerivation {
