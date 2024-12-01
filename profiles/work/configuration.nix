a5d7529c2f312ca68d849541db2234c3aa488796
Emmet <emmet@librephoenix.com>
2024-11-30 23:46:04 -0600

    Should hopefully improve build times

--- profiles/work/configuration.nix
+++ profiles/work/configuration.nix
@@ -37,6 +37,23 @@
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
+
+  # Substituters
+  nix.settings = {
+    substituters = [
+      "https://hyprland.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-substituters = [
+      "https://hyprland.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   nixpkgs.overlays = [
     (
       final: prev: {
