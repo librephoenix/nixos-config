b751878241b9febbec39978aa723e50fb5416c1c
Emmet <emmet@librephoenix.com>
2024-12-06 10:32:11 -0600

    Use hyprprofiles even on work system profile

--- user/wm/hyprland/hyprland.nix
+++ user/wm/hyprland/hyprland.nix
@@ -10,7 +10,7 @@ in
     })
     ../input/nihongo.nix
   ] ++
-  (if (systemSettings.profile == "personal") then
+  (if ((systemSettings.profile == "personal") || (systemSettings.profile == "work")) then
     [ (import ./hyprprofiles/hyprprofiles.nix {
         dmenuCmd = "fuzzel -d"; inherit config lib pkgs; })]
   else
