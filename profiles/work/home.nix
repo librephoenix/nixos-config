245c97ab1d6f0c887fd8e53d1adcba5b5e6b6dbc
Emmet <emmet@librephoenix.com>
2024-12-07 23:42:53 -0600

    Declarative substituter settings for user

--- profiles/work/home.nix
+++ profiles/work/home.nix
@@ -27,6 +27,16 @@
   ];
 
   nix.package = pkgs.nix;
+  nix.settings = {
+    extra-substituters = [
+      "https://hyprland.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   home.stateVersion = "22.11"; # Please read the comment before changing.
 
