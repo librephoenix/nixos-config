c68f5cdc15abbcf53426f359647a475d83d59005
Emmet <emmet@librephoenix.com>
2024-12-08 10:09:22 -0600

    Hopefully this fixes substituters?

--- profiles/work/home.nix
+++ profiles/work/home.nix
@@ -28,11 +28,13 @@
 
   nix.package = pkgs.nix;
   nix.settings = {
-    extra-substituters = [
+    substituters = [
+      "https://cache.nixos.org"
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
