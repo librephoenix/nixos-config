2c042240ea12b6b9ec2793d3082d67227070d028
Emmet <emmet@librephoenix.com>
2024-12-18 22:34:01 -0600

    Better double keypress rejection maybe?

--- user/wm/hyprland/hyprland.nix
+++ user/wm/hyprland/hyprland.nix
@@ -372,7 +372,7 @@ in
        input {
          kb_layout = us
          kb_options = caps:escape
-         repeat_delay = 350
+         repeat_delay = 450
          repeat_rate = 50
          accel_profile = adaptive
          follow_mouse = 2
