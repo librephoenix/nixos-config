e55281223f7c41dce4867f3ee528a53ed8ddbe18
ama <ponymushama@gmail.com>
2024-12-30 15:30:09 +0800

    change power setting

--- system/hardware/power.nix
+++ system/hardware/power.nix
@@ -16,14 +16,13 @@
       CPU_MAX_PERF_ON_BAT = 100;
 
       CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
+      CPU_BOOST_ON_BAT = 1;
 
       START_CHARGE_THRESH_BAT0 = 75;
       STOP_CHARGE_THRESH_BAT0 = 80;
 
       PLATFORM_PROFILE_ON_AC = "performance";
-      PLATFORM_PROFILE_ON_BAT = "balanced";
+      PLATFORM_PROFILE_ON_BAT = "performance";
     };
   };
-
 }
