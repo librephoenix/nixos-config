63bac87b76b14c9a6c0065de48a2d72d180a995c
Emmet <emmet@librephoenix.com>
2025-03-26 10:20:14 -0500

    Keep zsh when going into nix-shell

--- modules/user/shell/default.nix
+++ modules/user/shell/default.nix
@@ -15,6 +15,18 @@ in {
       enableAutosuggestions = true;
       syntaxHighlighting.enable = true;
       enableCompletion = true;
+      plugins = [
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.8.0";
+            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+          };
+        }
+      ];
       shellAliases = {
         phoenix = "sudo phoenix";
         ls = "eza --icons -l -T -L=1";
