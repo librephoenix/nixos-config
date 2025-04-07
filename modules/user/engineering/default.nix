94d0c3fd2c7800b929e501bba4cd490a6d315150
Emmet <emmet@librephoenix.com>
2025-04-07 10:05:14 -0500

    Fix Cura

--- modules/user/engineering/default.nix
+++ modules/user/engineering/default.nix
@@ -16,6 +16,17 @@ in {
       kicad
       cura-appimage
       arduino-ide
+      graphviz
     ];
+    xdg.desktopEntries.cura = lib.mkForce {
+      name = "Ultimaker Cura";
+      genericName = "3D Printing Software";
+      icon = "cura-icon";
+      exec = "cura -platformtheme gtk3 %u";
+      mimeType = [ "model/stl" "application/vnd.ms-3mfdocument" "application/prs.wavefront-obj" "image/bmp" "image/gif" "image/jpeg" "image/png" "text/x-gcode" "application/x-amf" "application/x-ply" "application/x-ctm" "model/vnd.collada+xml" "model/gltf-binary" "model/gltf+json" "model/vnd.collada+xml+zip" ];
+      terminal = false;
+      type = "Application";
+      prefersNonDefaultGPU = true;
+    };
   };
 }
