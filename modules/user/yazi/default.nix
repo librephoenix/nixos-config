1476305c98a5c4fbcaee52d48bd8b991d19a0782
Emmet <emmet@librephoenix.com>
2025-10-11 18:40:04 -0500

    Yazi improvements

--- modules/user/yazi/default.nix
+++ modules/user/yazi/default.nix
@@ -13,8 +13,41 @@ in {
     programs.yazi = {
       enable = true;
       enableZshIntegration = true;
+      theme.icon = {
+        dirs = [
+          { name = ".config"; text = ""; }
+          { name = ".git"; text = ""; }
+          { name = ".github"; text = ""; }
+          { name = ".npm"; text = ""; }
+          { name = "Desktop"; text = ""; }
+          { name = "Development"; text = ""; }
+          { name = "Documents"; text = ""; }
+          { name = "Downloads"; text = ""; }
+          { name = "Library"; text = ""; }
+          { name = "Movies"; text = ""; }
+          { name = "Music"; text = ""; }
+          { name = "Pictures"; text = ""; }
+          { name = "Public"; text = ""; }
+          { name = "Videos"; text = ""; }
+          { name = "nixos"; text = ""; }
+          { name = "Archive"; text = ""; }
+          { name = "Media"; text = ""; }
+          { name = "Podcasts"; text = ""; }
+          { name = "Drive"; text = ""; }
+          { name = "KP"; text = ""; }
+          { name = "Books"; text = ""; }
+          { name = "Games"; text = ""; }
+          { name = "Game Saves"; text = ""; }
+          { name = "Templates"; text = ""; }
+          { name = "Notes"; text = ""; }
+          { name = "Projects"; text = ""; }
+          { name = "Screenshots"; text = ""; }
+        ];
+      };
       keymap.manager.prepend_keymap = 
         [
+	        { run = "cd ~/Projects"; on = [ "g" "p" ]; desc = "Go to projects"; }
+	        { run = "cd ~/Screenshots"; on = [ "g" "s" ]; desc = "Go to screenshots"; }
           { run = "shell ' \"$@\"' --cursor=0 --interactive"; on = [ "@" ]; }
           { run = "hidden toggle"; on = [ "<C-h>" ]; }
           { run = "yank"; on = [ "y" "y" ]; }
