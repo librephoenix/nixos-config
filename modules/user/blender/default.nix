41e760d484013af85f2d98e4c001fe176966a83a
Emmet <emmet@librephoenix.com>
2025-10-11 22:14:11 -0500

    Setup for declarative blender prefs

--- modules/user/blender/default.nix
+++ modules/user/blender/default.nix
@@ -10,7 +10,15 @@ in {
   };
 
   config = {
-    home.packages = [ pkgs.blender-hip ];
+    home.packages =
+      [ pkgs.blender-hip
+        (pkgs.writeScriptBin "declarative-blender-prefs"
+          ''
+          #!/bin/sh
+          ${pkgs.blender-hip}/bin/blender --python ${./applyprefs.py};
+          '')
+      ];
+    home.file.".config/declarative-blender-prefs/applyprefs.py".source = ./applyprefs.py;
     home.file.".config/blender/extensions/node_pie.zip".source = builtins.fetchurl {
       url = "https://github.com/strike-digital/node_pie/releases/download/1.2.38/node_pie_1_2_38.zip";
       sha256 = "sha256:00kscj7dkl80kc482jg3kcw9vhr1n64n44ld2xncr6gxil679fk2";
